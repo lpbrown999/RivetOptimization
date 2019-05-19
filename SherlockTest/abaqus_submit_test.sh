e517eb4ffa1bb59083feb9506c13245a8f52afc0
lpbrown999 <lpbrown@stanford.edu>
2019-05-19 12:07:48 -0700

    More testing, no result

--- SherlockTest/abaqus_submit_test.sh
+++ SherlockTest/abaqus_submit_test.sh
@@ -1,26 +1,22 @@
 #!/bin/bash
 #Set up resources
-#SBATCH -N 1 						#Node count
-#SBATCH --ntasks-per-node=1			#threads
+#SBATCH -N 1 						#Number of nodes
+#SBATCH --ntasks-per-node=1 		#How many cores required. Will checkout many tokens
 #SBATCH --job-name=abaqus_test		#Name of slurm job
 #SBATCH --time=10:00				#Max time
-#SBATCH --ntasks=1					
-#SBATCH --cpus-per-task=1
-#SBATCH --mem-per-cpu=4G
-#SBATCH -o process_%j.out
-#SBATCH -e process_%j.err
+#SBATCH --mem=4G
+#SBATCH --output=test_output_%j.output
+#SBATCH --error=test_error_%j.error
 
-#Change to execution directory
-#Run job
 #Fix for "<IBM Platform MPI>: : warning, dlopen of libhwloc.so failed (null)/lib/linux_amd64/libhwloc.so: cannot open shared object file: No such file or directory"
-# unset LD_LIBRARY_PATH
-# unset SLURM_GTIDS
+echo $SLURM_GTIDS
+unset SLURM_GTIDS
+echo "Attempted to unset SLURM_GTIDS"
+echo $SLURM_GTIDS
 
-srun /home/groups/fkchang/SIMULIA_Abaqus_CAE_2019/linux_a64/code/bin/ABQLauncher job=Job-1 input=Job-1 scratch=. interactive 
+#Run the job
+srun /home/groups/fkchang/SIMULIA_Abaqus_CAE_2019/linux_a64/code/bin/ABQLauncher job=Job-1 input=Job-1 \
+	verbose=3 mp_mode=mpi interactive scratch=. 
 
 #When done, copy ODB from execution directory to where we started from
-yourfilenames="ls ./*.*"
-for eachfile in $yourfilenames
-do
-   echo $eachfile
-done
+echo "Reached end of bash script"
