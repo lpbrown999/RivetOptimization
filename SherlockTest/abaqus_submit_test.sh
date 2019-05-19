b1345cce8d470b3a6d7094efc746760148145e21
lpbrown999 <lpbrown@stanford.edu>
2019-05-19 14:01:41 -0700

    Figured out how to work Sherlock

--- SherlockTest/abaqus_submit_test.sh
+++ SherlockTest/abaqus_submit_test.sh
@@ -4,19 +4,14 @@
 #SBATCH --ntasks-per-node=1 		#How many cores required. Will checkout many tokens
 #SBATCH --job-name=abaqus_test		#Name of slurm job
 #SBATCH --time=10:00				#Max time
-#SBATCH --mem=4G
-#SBATCH --output=test_output_%j.output
-#SBATCH --error=test_error_%j.error
+#SBATCH --mem=4G					#Memory requested
+#SBATCH --output=log_%j.output		#output log
 
 #Fix for "<IBM Platform MPI>: : warning, dlopen of libhwloc.so failed (null)/lib/linux_amd64/libhwloc.so: cannot open shared object file: No such file or directory"
-echo $SLURM_GTIDS
 unset SLURM_GTIDS
-echo "Attempted to unset SLURM_GTIDS"
-echo $SLURM_GTIDS
 
 #Run the job
-srun /home/groups/fkchang/SIMULIA_Abaqus_CAE_2019/linux_a64/code/bin/ABQLauncher job=Job-1 input=Job-1 \
-	verbose=3 mp_mode=mpi interactive scratch=. 
+/home/groups/fkchang/SIMULIA_Abaqus_CAE_2019/linux_a64/code/bin/ABQLauncher job=Job-1 input=Job-1.inp mp_mode=mpi interactive scratch=. 
 
 #When done, copy ODB from execution directory to where we started from
 echo "Reached end of bash script"
